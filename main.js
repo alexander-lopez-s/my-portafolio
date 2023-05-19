e0e2b416b0731d089088aedea8fd1efc51cbe58d
Cristobal Alexander Lopez Segovia <alxlpz@outlook.com>
2023-05-19 13:02:24 +0200

    Code fixes

--- main.js
+++ main.js
@@ -3,9 +3,12 @@ let images = [
   "./public/slide1.png",
   "./public/slide2.png",
   "./public/slide3.png",
+  "./public/slide4.png",
+  "./public/slide5.png",
+  "./public/slide6.png",
 ];
 
 setInterval(function () {
-  let random = Math.floor(Math.random() * 3);
+  let random = Math.floor(Math.random() * 5);
   image.src = images[random];
-}, 4000);
+}, 3000);
